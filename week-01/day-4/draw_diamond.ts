60b0c7c7fd870a351b444a550f61482921e5b38b
aly1899 <alpar1899@gmail.com>
2019-04-04 21:01:56 +0200

    adding loop for lines and for spaces

---
+++ week-01/day-4/draw_diamond.ts
@@ -0,0 +1,17 @@
+'use strict';
+
+// var lineCount: number = 4;
+// var astrix:string = "*"
+// for (let i=1;i<=lineCount;i++){
+//     console.log(astrix.repeat(i));
+// }
+
+var lineCount: number = 7;
+var line:string = ""
+for (let i=1;i<=Math.round(lineCount/2);i+=2){
+    for (let k =1;k<Math.round(lineCount/4)){
+        line    
+    }
+        line+="*"
+    console.log(line)
+}
