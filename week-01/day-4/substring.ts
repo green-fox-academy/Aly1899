e80a16da574ac2b68f22258041a7eb57d5026ff0
aly1899 <alpar1899@gmail.com>
2019-04-05 10:55:55 +0200

    function to find a string in another and return the starting index

---
+++ week-01/day-4/substring.ts
@@ -0,0 +1,11 @@
+'use strict';
+
+//  Create a function that takes two strings as a parameter
+//  Returns the starting index where the second one is starting in the first one
+//  Returns `-1` if the second string is not in the first one
+
+function substr(str: string, keyword: string) : number {
+    return str.indexOf(keyword);
+}
+
+console.log(substr("asdfgh","df"));
