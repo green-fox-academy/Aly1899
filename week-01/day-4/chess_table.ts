9abc65158fe0c10bf003fa7610e4f9c109e52048
aly1899 <alpar1899@gmail.com>
2019-04-05 10:52:45 +0200

    app ready- create a scalable chess table

---
+++ week-01/day-4/chess_table.ts
@@ -0,0 +1,21 @@
+'use strict';
+
+let lineCount:number=9;
+let field:string="";
+
+for (let i=1;i<=lineCount;i++){
+    if (i%2===0){
+        for (let k=1;k<=lineCount;k+=2){
+            field+=" %";
+        }
+        console.log(field);
+        field="";
+    }else {
+        for (let k=1;k<=lineCount;k+=2){
+            field+="% ";
+        }
+        console.log(field);
+        field="";
+    }
+
+}
