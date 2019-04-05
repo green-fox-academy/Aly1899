a193a75cf150703836cc339018081f28388639fe
aly1899 <alpar1899@gmail.com>
2019-04-05 10:23:45 +0200

    test file , no content

--- week-01/day-4/first_ts.ts
+++ week-01/day-4/first_ts.ts
@@ -1,6 +1,4 @@
 "use strict"
 
-console.log('All the king\'s horses and all the king\'s men');
-console.log('Humpty Dumpty had a great fall.');
-console.log('Humpty Dumpty sat on a wall,');
-console.log('Couldn\'t put Humpty together again.');
+let myFavouriteNuber: number=9
+console
