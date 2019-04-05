a4052e6289f77f2b70bec945996cf50a5a5aaac8
aly1899 <alpar1899@gmail.com>
2019-04-05 10:22:37 +0200

    added bottom part of the diamond

--- week-01/day-4/draw_diamond.ts
+++ week-01/day-4/draw_diamond.ts
@@ -6,12 +6,29 @@
 //     console.log(astrix.repeat(i));
 // }
 
-var lineCount: number = 7;
+var lineCount: number = 36;
 var line:string = ""
-for (let i=1;i<=Math.round(lineCount/2);i+=2){
-    for (let k =1;k<Math.round(lineCount/4)){
-        line    
+var space=''
+for (let i=1;i<=Math.round(lineCount/2);i++){
+    for (let k =lineCount-i;k>=0;k--){
+        space +=" "   
     }
-        line+="*"
-    console.log(line)
+    for (let k =1;k<=2*i-1;k++){ 
+        line+="*";
+    }
+    console.log(space + line)
+    space=""
+    line=""
+}
+
+for (let i=Math.floor(lineCount/2);i>=1;i--){
+    for (let k =0;k<=lineCount-i;k++){
+        space +=" "   
+    }
+    for (let k =2*i-1;k>=1;k--){ 
+        line+="*";
+    }
+    console.log(space + line)
+    space=""
+    line=""
 }
