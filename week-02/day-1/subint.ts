c58267305db08fef1fae92430513c9c1433642c4
aly1899 <alpar1899@gmail.com>
2019-04-08 17:24:29 +0200

    feat(subint.ts)add - return the indeses of elements in an array which  contain the given number

---
+++ week-02/day-1/subint.ts
@@ -0,0 +1,12 @@
+'use strict'
+
+function subint(item:number,list: number[]) {
+    let itemIndeses:number[]=[]
+    for (let i=0;i<list.length;i++){
+        if (list[i].toString().indexOf(item.toString())>-1) {
+            itemIndeses.push(i)
+        }
+    }
+    return itemIndeses;
+}
+console.log(subint(9,[1, 11, 34, 52, 61]));
