5d52538c55b8f93a622a1874fff15a2c8cfb5ac2
aly1899 <alpar1899@gmail.com>
2019-04-08 22:06:26 +0200

    corr(matrix.ts) -- array has been corrected and matrix displayed as string

--- week-02/day-1/matrix.ts
+++ week-02/day-1/matrix.ts
@@ -1,9 +1,9 @@
 'use strict'
 
-let numberOfElements: number = 7;
+let numberOfElements: number =4;
 let squareMatrix = new Array(numberOfElements);
 for (let i = 0; i < numberOfElements; i++) {
-    squareMatrix[i] = new Array(numberOfElements)
+    squareMatrix[i] = new Array()
     for (let j = numberOfElements - 1; j >= 0; j--) {
         if (j === numberOfElements - i - 1) {
             squareMatrix[i].unshift(1);
@@ -12,5 +12,6 @@ for (let i = 0; i < numberOfElements; i++) {
         }
     }
 }
-
-console.log(squareMatrix);
+for (let i=0;i<numberOfElements;i++){
+    console.log(squareMatrix[i].join(''));
+}
