47c318e72d8dc9c27aa8b9a8cdad5fb8484e862a
Alpar <alpar1899@gmail.com>
2019-04-10 08:38:51 +0200

    feat(map2.ts)
    add - working with objects

--- week-02/day-2/map2.ts
+++ week-02/day-2/map2.ts
@@ -1,6 +1,6 @@
 'use strict';
 
-let myObject = {
+let myObject:any = {
     '978-1-60309-452-8': 'A Letter to Jo',
     '978-1-60309-459-7': 'Lupus',
     '978-1-60309-444-3': 'Red Panda and Moon Bear',
