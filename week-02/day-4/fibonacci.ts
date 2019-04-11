eb049f1e3c63e416a3bac2276cab43b1b41f61cb
Alpar <alpar1899@gmail.com>
2019-04-11 15:30:31 +0200

    feat(fibonacci.ts) add : return the nth element of Fibonacci sequence

---
+++ week-02/day-4/fibonacci.ts
@@ -0,0 +1,12 @@
+'use strict'
+
+function fibonacci(n:number):number{
+     if (n===1){
+        return 0;
+    } 
+    if (n===2){
+        return 1;
+    }
+    return fibonacci(n-1)+fibonacci(n-2)
+}
+console.log(fibonacci(8))
