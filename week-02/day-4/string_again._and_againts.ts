a134e1d2692da939107315c189422658936a1093
Alpar <alpar1899@gmail.com>
2019-04-11 14:28:41 +0200

    feat(string_again_and_again) add: add a * after each letter in a string

---
+++ week-02/day-4/string_again._and_againts.ts
@@ -0,0 +1,11 @@
+'use strict'
+
+function add_star(str:string):string{
+    let strClone:string=str
+    let strResult:string
+    if (strClone.length){
+        return strResult;
+    }
+    return strResult+=strClone.shift()
+}
+console.log(remove_x('abcxabcxabcxXabcX'))
