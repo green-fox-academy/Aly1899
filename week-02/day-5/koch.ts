d46a8e4db3e37a734d80d5de007146da0c8705c8
Alpar <alpar1899@gmail.com>
2019-04-18 22:09:34 +0200

    feat(knoch.ts) add: draw a knoch line fractal

--- week-02/day-5/koch.ts
+++ week-02/day-5/koch.ts
@@ -3,19 +3,6 @@
 const canvas = document.querySelector('.main-canvas') as HTMLCanvasElement;
 const ctx = canvas.getContext('2d');
 
-function koch(x: number, y: number, s:number,i: number) {
-    let colors:string[]=['green','blue','orange','purple']
-    if (i<1){
-        return
-    }
-    ctx.beginPath()
-    ctx.moveTo(x,y)
-    ctx.lineTo(x+s,y)
-    ctx.lineTo(x+s+s*Math.sin(Math.PI/6),y-s*Math.cos(Math.PI/6))
-    ctx.lineTo(x+s+2*s*Math.sin(Math.PI/6),y)
-    ctx.lineTo(x+2*s+2*s*Math.sin(Math.PI/6),y)
-    // ctx.lineTo(x+s,y)
-    ctx.stroke()
-    koch(x+2*s+2*s*Math.sin(Math.PI/6),y,30,i-1)
-}
-koch(0, 100, 30,1);
+
+function forward()
+// koch(0, 100, 10,7);
