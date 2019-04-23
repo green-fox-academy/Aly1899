8e4329bbc79686fde02c7fbdde3a7a197118da82
Alpar <alpar1899@gmail.com>
2019-04-23 22:26:45 +0200

    adj(flower) - waterNeed changed

--- week-04/day-2/Garden/flower.ts
+++ week-04/day-2/Garden/flower.ts
@@ -5,10 +5,10 @@ import {Plant} from './plant'
 export class Flower extends Plant{
 // type:string='Flower' 
 // waterAbsorbtion:number =0.75
-// waterNeed:number=7.5
+// waterNeed:number=5
 
   constructor(name:string){
-    super('Flower',name,0.75,7.5,0)
+    super('Flower',name,0.75,5,0)
   }
 
 }
