f8567acac9aa1cf60f01e356544a07e27e2dd3d6
Alpar <alpar1899@gmail.com>
2019-05-24 11:27:22 +0200

    feat (app.js) add: adding query for all posts by user

--- week-07/day-5/Reddit/app.js
+++ week-07/day-5/Reddit/app.js
@@ -5,6 +5,8 @@ const mySql=require('mysql');
 const app=express();
 const PORT=3000;
 
+app.use(express.json());
+
 const reddit=mySql.createConnection({
   host:'localhost',
   user:'root',
@@ -13,7 +15,26 @@ const reddit=mySql.createConnection({
 });
 
 app.get('/posts',(req,res)=>{
-  res.send('hello')
+  let userID=req.query.userID
+  let allPostsQuery=`
+  SELECT p.post_id,title,url,create_at,score,u.user_name, vote
+  FROM posts p
+  LEFT JOIN users u
+  ON u.user_id=p.user_id
+  LEFT JOIN 
+    (SELECT * FROM vote
+    WHERE user_id=${userID}) v
+  ON v.user_id=p.user_id AND v.post_id=v.post_id;
+  `
+  console.log(userID)
+  reddit.query(allPostsQuery,(err,result)=> {
+    if (err){
+      console-log(`Error in connection to database: ${err}`)
+    }
+
+    console.log(result)
+    res.json(result)
+  })
 })
 
 app.listen(PORT,()=>{
