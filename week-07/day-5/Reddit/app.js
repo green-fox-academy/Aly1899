b518d33ab04e86dc36d33d6b01f919ae235a441b
Alpar <alpar1899@gmail.com>
2019-05-24 14:00:06 +0200

    feat(app.js) add: post request to insert posts

--- week-07/day-5/Reddit/app.js
+++ week-07/day-5/Reddit/app.js
@@ -26,15 +26,46 @@ app.get('/posts',(req,res)=>{
     WHERE user_id=${userID}) v
   ON v.user_id=p.user_id AND v.post_id=v.post_id;
   `
-  console.log(userID)
   reddit.query(allPostsQuery,(err,result)=> {
     if (err){
       console-log(`Error in connection to database: ${err}`)
     }
-
-    console.log(result)
     res.json(result)
   })
+})
+
+
+app.post('/posts',(req,res)=>{
+  let title=req.body.title
+  console.log(title)
+  let url=req.body.url
+  console.log(url)
+  let userID=req.query.userid
+  console.log(userID)
+  let qry=`INSERT INTO posts (title,url,user_id) VALUES('${title}','${url}','${userID}');`
+  console.log(qry)
+  reddit.query(qry,(err,result)=>{
+    if (err){
+      console.log('Error in INSERT to DB'+err)
+    }
+    reddit.query(
+      `
+      SELECT p.post_id,title,url,create_at,score,u.user_name, vote
+      FROM posts p
+      LEFT JOIN users u
+      ON u.user_id=p.user_id
+      LEFT JOIN 
+        (SELECT * FROM vote
+        WHERE user_id=${userID}) v
+      ON v.user_id=p.user_id AND v.post_id=v.post_id
+      WHERE p.post_id=${result.insertId};`,(err,responseResult)=>{
+        if (err){
+          console.log('Error in POST RESPONSE SELECT');
+        }
+      res.send(responseResult)
+      })
+  })
+  console.log(req.body)
 })
 
 app.listen(PORT,()=>{
