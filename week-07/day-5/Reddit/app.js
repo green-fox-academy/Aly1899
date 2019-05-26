b0d60258231e551a8eddb0a88d121f0a86771702
Alpar <alpar1899@gmail.com>
2019-05-26 10:17:45 +0200

    feat(app.js) add all routes added

--- week-07/day-5/Reddit/app.js
+++ week-07/day-5/Reddit/app.js
@@ -15,17 +15,21 @@ const reddit=mySql.createConnection({
 });
 
 app.get('/posts',(req,res)=>{
-  let userID=req.query.userID
+  let userID=req.body.userid
   let allPostsQuery=`
-  SELECT p.post_id,title,url,create_at,score,u.user_name, vote
+  SELECT p.post_id,title,url,create_at,s.score,u.user_name,vote
   FROM posts p
   LEFT JOIN users u
   ON u.user_id=p.user_id
   LEFT JOIN 
-    (SELECT * FROM vote
-    WHERE user_id=${userID}) v
-  ON v.user_id=p.user_id AND v.post_id=v.post_id;
-  `
+    (SELECT * FROM vote v
+    WHERE user_id=${userID} ) v
+  ON v.user_id=p.user_id AND v.post_id=p.post_id
+  LEFT JOIN 
+    (SELECT post_id,sum(vote) AS score FROM vote
+    GROUP BY post_id) s
+  ON s.post_id=p.post_id
+    `
   reddit.query(allPostsQuery,(err,result)=> {
     if (err){
       console-log(`Error in connection to database: ${err}`)
@@ -37,18 +41,115 @@ app.get('/posts',(req,res)=>{
 
 app.post('/posts',(req,res)=>{
   let title=req.body.title
-  console.log(title)
   let url=req.body.url
-  console.log(url)
-  let userID=req.query.userid
+  let userID=req.body.userid
   console.log(userID)
   let qry=`INSERT INTO posts (title,url,user_id) VALUES('${title}','${url}','${userID}');`
-  console.log(qry)
   reddit.query(qry,(err,result)=>{
     if (err){
       console.log('Error in INSERT to DB'+err)
     }
+    console.log(result.insertId)
     reddit.query(
+      `
+      SELECT p.post_id,title,url,create_at,IFNULL(s.score,0) AS score,u.user_name, vote
+      FROM posts p
+      LEFT JOIN users u
+      ON u.user_id=p.user_id
+      LEFT JOIN 
+        (SELECT * FROM vote
+        WHERE user_id=${userID}) v
+      ON v.user_id=p.user_id AND v.post_id=p.post_id
+      LEFT JOIN 
+        (SELECT post_id,sum(vote) AS score FROM vote
+        GROUP BY post_id) s
+      ON s.post_id=p.post_id
+      WHERE p.post_id=${result.insertId};`,(err,responseResult)=>{
+        if (err){
+          console.log('Error in POST RESPONSE SELECT');
+        }
+      res.send(responseResult)
+      })
+  })
+})
+
+
+app.put('/posts/:postid/upvote',(req,res)=>{
+  let postID=req.params.postid
+  let userID=req.query.userid
+  let voteCheck
+  reddit.query(`
+    SELECT COUNT(*) FROM vote
+    WHERE user_id=${userID} AND post_id=${postID};
+  `,(err,voteCheckResult)=>{
+    voteCheckResult
+  })
+  reddit.query(
+    `INSERT INTO vote(user_id,post_id,vote) 
+    VALUES
+    ('${userID}','${postID}','1')`,(err,result)=>{
+      if (err){
+        console.log('Error in PUT UPVOTE INSERT')
+      }
+      reddit.query(
+      `
+      SELECT p.post_id,title,url,create_at,score,u.user_name, vote
+      FROM posts p
+      LEFT JOIN users u
+      ON u.user_id=p.user_id
+      LEFT JOIN 
+        (SELECT * FROM vote
+        WHERE user_id=${userID} AND post_id=${postID}) v
+      ON v.user_id=p.user_id AND v.post_id=v.post_id
+      WHERE p.post_id=${postID};`,(err,responseResult)=>{
+        if (err){
+          console.log('Error in POST RESPONSE SELECT');
+        }
+      res.send(responseResult)
+      })
+    })
+  })
+
+  app.put('/posts/:postid/downvote',(req,res)=>{
+    
+
+
+    let postID=req.params.postid
+    let userID=req.query.userid
+    reddit.query(
+      `INSERT INTO vote(user_id,post_id,vote) 
+      VALUES
+      ('${userID}','${postID}','-1')`,(err,result)=>{
+        if (err){
+          console.log('Error in PUT DOWNVOTE INSERT')
+        }
+        reddit.query(
+        `
+        SELECT p.post_id,title,url,create_at,score,u.user_name, vote
+        FROM posts p
+        LEFT JOIN users u
+        ON u.user_id=p.user_id
+        LEFT JOIN 
+          (SELECT * FROM vote
+          WHERE user_id=${userID}) v
+        ON v.user_id=p.user_id AND v.post_id=v.post_id
+        WHERE p.post_id=${postID};`,(err,responseResult)=>{
+          if (err){
+            console.log('Error in POST RESPONSE SELECT');
+          }
+        res.send(responseResult)
+        })
+      })
+    })
+
+
+    app.delete('/posts/:postid',(req,res)=>{
+      let postID=req.params.postid
+      let userID=req.headers.userid
+      let tempRes
+      console.log(userID)
+      // req.header('')
+      reddit.query(
       `
       SELECT p.post_id,title,url,create_at,score,u.user_name, vote
       FROM posts p
@@ -58,15 +159,58 @@ app.post('/posts',(req,res)=>{
         (SELECT * FROM vote
         WHERE user_id=${userID}) v
       ON v.user_id=p.user_id AND v.post_id=v.post_id
-      WHERE p.post_id=${result.insertId};`,(err,responseResult)=>{
+      WHERE p.post_id=${postID};`,(err,responseResult)=>{
+        if (err){
+          console.log('Error in DELETE SELECT')
+        }
+        // res.type('json')
+        tempRes=responseResult
+      })
+      reddit.query(
+        `DELETE FROM posts
+        WHERE post_id=${postID};`,(err,result)=>{
+          if (err){
+            console.log('Error in DELETE INSERT post')
+          }
+        res.json(tempRes) 
+        })
+    })
+
+app.put('/posts/:postid',(req,res)=>{
+  let title=req.body.title
+  let url=req.body.url
+  let postID=req.params.postid
+  let userID=req.body.userid
+  let qry=
+  `UPDATE posts 
+  SET
+  title='${title}',
+  url='${url}'
+  where post_id=${postID};
+  `
+  reddit.query(qry,(err,result)=>{
+    if (err){
+      console.log('Error in PUT UPDATE to DB'+err)
+    }
+    reddit.query(
+      `
+      SELECT p.post_id,title,url,create_at,score,u.user_name, vote
+      FROM posts p
+      LEFT JOIN users u
+      ON u.user_id=p.user_id
+      LEFT JOIN 
+        (SELECT * FROM vote
+        WHERE user_id=${userID} AND post_id=${postID}) v
+      ON v.user_id=p.user_id AND v.post_id=v.post_id
+      WHERE p.post_id=${postID};`,(err,responseResult)=>{
         if (err){
           console.log('Error in POST RESPONSE SELECT');
         }
       res.send(responseResult)
       })
   })
-  console.log(req.body)
 })
+    
 
 app.listen(PORT,()=>{
   console.log(`Listening to port: ${PORT}`);
